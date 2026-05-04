c8a14261bd030dc26063bed941eb5a627f4850f5
Anderson Lopes <lopesdea91@gmail.com>
2026-05-04 19:55:04 -0300

    implement delete and update functionality for pizzas

--- cmd/main.go
+++ cmd/main.go
@@ -17,6 +17,8 @@ func main() {
 	router.GET("/pizzas", getPizzas)
 	router.POST("/pizzas", postPizzas)
 	router.GET("/pizzas/:id", getPizzasByID)
+	router.DELETE("/pizzas/:id", deletePizzasByID)
+	router.PUT("/pizzas/:id", updatePizzasByID)
 	router.Run()
 }
 
@@ -80,4 +82,51 @@ func savePizza() {
 	if err := encoder.Encode(pizzas); err != nil {
 		fmt.Println("Error encoding JSON:", err)
 	}
+}
+
+func deletePizzasByID(c *gin.Context){
+	idParam:= c.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"erro": err.Error()})
+		return 
+	}
+
+	for i, p := range pizzas {
+		if p.ID == id {
+			pizzas = append(pizzas[:i], pizzas[i+1:]...)
+			savePizza()
+			c.JSON(200, gin.H{"message": "Pizza deleted"})}
+			return 
+	}	
+	
+	c.JSON(200, gin.H{"method": "pizza not found"})
+}
+
+func updatePizzasByID(c * gin.Context){
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updatedPizza models.Pizza
+	if err := c.ShouldBindJSON(&updatedPizza); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, p := range pizzas {
+		if p.ID == id {
+			pizzas[i] = updatedPizza
+			pizzas[i].ID = id
+			savePizza()
+			c.JSON(200, pizzas[i])
+			return
+		}
+	}
+	c.JSON(200, gin.H{"method": "pizza not found"})
 }
