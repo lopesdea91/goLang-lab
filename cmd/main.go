02aaf23b79ff95dd5182d6f550cd553298493d69
Anderson Lopes <lopesdea91@gmail.com>
2026-05-08 07:05:00 -0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,8 @@
 package main
 
-import (
-	"pizzaria/internal/data"
-	"pizzaria/internal/handler"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	data.LoadPizzas()
-	router := gin.Default()
-	router.GET("/pizzas", handler.GetPizzas)
-	router.POST("/pizzas", handler.PostPizzas)
-	router.GET("/pizzas/:id", handler.GetPizzasByID)
-	router.DELETE("/pizzas/:id", handler.DeletePizzasByID)
-	router.PUT("/pizzas/:id", handler.UpdatePizzasByID)
-
-	router.POST("/pizzas/:id/reviews", handler.PostReview)
-
-	router.Run()
+	r := gin.Default()
+	r.Run(":5000")
 }
--- internal/models/review.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Review struct {
-	Rating  int    `json:"rating"` // 1 a 5
-	Comment string `json:"comment"`
-}
