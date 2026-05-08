b1fe9c366bd70f0ee0d3f8ac7c0770b33b18d3dc
Anderson Lopes <lopesdea91@gmail.com>
2026-05-08 07:13:37 -0300

    retorno /alunos

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ExiteTodosAlunos(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"id":   "1",
+		"nome": "anderson",
+	})
+}
 
 func main() {
 	r := gin.Default()
+
+	r.GET("alunos", ExiteTodosAlunos)
+
 	r.Run(":5000")
 }
